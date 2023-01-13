d55e173ed43353009d02caf42054944d18021d4f
Szymon W <szyszka>
2023-01-14 00:04:20 +0100

    Tested printf and led

--- main.c
+++ main.c
@@ -2,9 +2,18 @@
 #include "pico/stdlib.h"
 
 int main() {
+    stdio_init_all();
+
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
 
     while(true) {
-        printf("Test print");
+        printf("Test print\n");
+
+        gpio_put(25, 1);
+        sleep_ms(1000);
+
+        gpio_put(25, 0);
         sleep_ms(1000);
     }
 
