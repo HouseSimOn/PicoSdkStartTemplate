38e979858bfa6077922946b923c94441b00961c9
Szymon W <jakszyszka@interia.pl>
2023-01-24 22:34:11 +0100

    Added led blinking for pico_w

--- main.c
+++ main.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#if defined RASPBERRYPI_PICO_W
+#include "pico/cyw43_arch.h"
+#endif
 
 bool timerCallback (repeating_timer_t *rt) {
     printf("Timer callback\n");
     return true;
 }
 
-// #if defined RASPBERRYPI_PICO_W
-// // Pico-W
-// #else
-// // Pico
-// #endif
+#if defined RASPBERRYPI_PICO_W
+// Pico-W
+void static setLedPin(bool State) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, State);
+}
+#else
+// Pico
+void static setLedPin(bool State) {
+    gpio_put(25, State);
+}
+#endif
 
 repeating_timer_t timer;
 
 int main() {
     stdio_init_all();
 
+#if defined RASPBERRYPI_PICO_W
+    cyw43_arch_init();
+#else
     gpio_init(25);
     gpio_set_dir(25, GPIO_OUT);
+#endif
 
     add_repeating_timer_ms(1000, timerCallback, NULL, &timer);
 
     while(true) {
         printf("Test print\n");
 
-        gpio_put(25, 1);
+        // gpio_put(25, 1);
+        setLedPin(true);
         sleep_ms(1000);
 
-        gpio_put(25, 0);
+        // gpio_put(25, 0);
+        setLedPin(false);
         sleep_ms(1000);
     }
 
