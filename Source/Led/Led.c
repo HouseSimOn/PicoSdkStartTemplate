c1a394bc7223a2c896eb57890d14393ac2fa4e1c
Szymon W <jakszyszka@interia.pl>
2023-01-25 22:34:34 +0100

    Added Led module

---
+++ Source/Led/Led.c
@@ -0,0 +1,28 @@
+#include "Led.h"
+
+#if defined RASPBERRYPI_PICO_W
+
+void Led_Init() {}
+
+#else
+
+void Led_Init() {
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+}
+
+#endif
+
+#if defined RASPBERRYPI_PICO_W
+
+void Led_SetState(bool State) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, State);
+}
+
+#else
+
+void Led_SetState(bool State) {
+    gpio_put(25, State);
+}
+
+#endif
